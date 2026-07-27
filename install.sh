473c72fabcd6019a2480b5b66c9bfa52d26174b7
dobmen <78257020+dobmen@users.noreply.github.com>
2026-07-27 12:52:38 +0200

    Fix udev touch calibration rule to explicitly target Wacom HID hardware names

--- install.sh
+++ install.sh
@@ -53,8 +53,8 @@ systemctl enable seatd
 
 echo "[3.8/8] Applying global touchscreen calibration matrix..."
 cat << 'EOF' | tee /etc/udev/rules.d/99-touchscreen.rules > /dev/null
-ACTION=="add|change", KERNEL=="event[0-9]*", ENV{ID_INPUT_TOUCHSCREEN}=="1", ENV{LIBINPUT_CALIBRATION_MATRIX}="0 1 0 -1 0 1"
-ACTION=="add|change", KERNEL=="event[0-9]*", ENV{ID_INPUT_TABLET}=="1", ENV{LIBINPUT_CALIBRATION_MATRIX}="0 1 0 -1 0 1"
+ACTION=="add|change", SUBSYSTEM=="input", ATTRS{name}=="Wacom HID 4808 Finger", ENV{LIBINPUT_CALIBRATION_MATRIX}="0 1 0 -1 0 1"
+ACTION=="add|change", SUBSYSTEM=="input", ATTRS{name}=="Wacom HID 4808 Pen", ENV{LIBINPUT_CALIBRATION_MATRIX}="0 1 0 -1 0 1"
 EOF
 udevadm control --reload-rules && udevadm trigger
 
